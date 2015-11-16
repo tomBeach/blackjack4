754b4163e9d7ca72bf46f13eede22528df6b45c3
tomBeach <tbeach2k@gmail.com>
2015-11-16 16:28:09 -0500

    merge/modify/unmerge/unmodify working

--- test.js
+++ test.js
@@ -56,6 +56,11 @@ function initGame() {
         /* input:  */ null,
         /* image:  */ null);
 
+    var player2_scr = new Screen();
+    var player3_scr = new Screen();
+    var dealer_scr = new Screen();
+    var scoreboard_scr = new Screen();
+
     var splash = new Screen(
         /* name:   */ "splash",
         /* type:   */ "game",
@@ -132,9 +137,9 @@ function initGame() {
         this.handOver = ["borderH_1", "borderV_1", "pName_1", "pScore_1", "pBank_1"];
     }
 
-    // ======= ======= ======= getNextSubScreen ======= ======= =======
-    Display.prototype.getNextSubScreen = function() {
-        console.log("getNextSubScreen");
+    // ======= ======= ======= getNextSubscreen ======= ======= =======
+    Display.prototype.getNextSubscreen = function() {
+        console.log("getNextSubscreen");
     }
 
     // ======= ======= ======= getNextScreen ======= ======= =======
@@ -157,15 +162,15 @@ function initGame() {
         } else {
             prevScreen = game.currentScreen;
             prevScreenName = prevScreen.name;
-            for (var i = 0; i < game.allScreens.length; i++) {
-                nextName = game.allScreens[i].name;
+            for (var i = 0; i < game.screenObjectsArray.length; i++) {
+                nextName = game.screenObjectsArray[i].name;
                 if (nextName == prevScreenName) {
                     nextScreenIndex = i + 1;
                     break
                 }
             }
         }
-        nextScreen = game.allScreens[nextScreenIndex];
+        nextScreen = game.screenObjectsArray[nextScreenIndex];
 
         // == identify items to remove, add or keep unchanged
         var changeItemsArray = this.processPrevNextItems(prevScreen, nextScreen);
@@ -243,7 +248,7 @@ function initGame() {
                 nextItemName = nextItem.name;
                 var found = $.inArray(nextItemName, REMprevNames)
                 if (found > -1) {
-                    REMprevItems.push(nextItems);
+                    REMprevItems.push(nextItem);
                 }
             }
         }
@@ -262,20 +267,31 @@ function initGame() {
     Display.prototype.processScreenItems = function(removeItemsArray, addItemsArray) {
         console.log("processScreenItems");
 
+        var nextItem, nextType, indexCell, newTextInput;
+
         // == remove prev items/add next items
         if (removeItemsArray) {
             for (var j = 0; j < removeItemsArray.length; j++) {
                 nextItem = removeItemsArray[j];
                 nextType = nextItem.type;
-                display.deActivateNextItem(nextItem);
-                display.unModifyGridRegion(nextItem);
+                indexCell = display.unModifyGridRegion(nextItem);
+                if ((nextType == "btn") || (nextType == "slider")) {
+                    display.deActivateNextItem(nextItem, indexCell);
+                }
             }
         }
         for (var j = 0; j < addItemsArray.length; j++) {
             nextItem = addItemsArray[j];
             nextType = nextItem.type;
-            display.modifyGridRegion(nextItem);
-            display.activateNextItem(nextItem);
+            indexCell = display.modifyGridRegion(nextItem);
+            if (nextType == "input") {
+                newTextInput = "<input id='" + nextItem.name + "Input' class='" + nextItem.class + "' type='text' value='Tom'>"
+                $(indexCell).append(newTextInput);
+                $(newTextInput).attr("id", nextItem.name);
+            }
+            if ((nextType == "btn") || (nextType == "slider")) {
+                display.activateNextItem(nextItem, indexCell);
+            }
         }
     }
 
@@ -299,7 +315,7 @@ function initGame() {
             totalSpanOffset = nextCol - colspans - rowspans;
 
             if (regionType == "merge") {
-                for (var col = 0; col < (whichItem.iW); col++) {
+                for (var col = 0; col < whichItem.iW; col++) {
                     if (((row == 0) && (col == 1))) {
                         totalColOffset = totalSpanOffset + col;
                     }
@@ -317,44 +333,142 @@ function initGame() {
                     }
 
                 }
+
+                // == set row/colspans on index cell to fill space
+                $(indexCell).attr("colSpan", whichItem.iW);
+                $(indexCell).attr("rowSpan", whichItem.iH);
+                $(indexCell).addClass(whichItem.class);
+                if (whichItem.type != "input") {
+                    $(indexCell).attr("id", whichItem.name);
+                }
+
+                // == modify item class via iem class parameters
+            } else {
+                for (var col = 0; col < whichItem.iW; col++) {
+                    nextCell = $(nextRowObject).children()[totalSpanOffset + col];
+                    $(nextCell).removeClass();
+                    $(nextCell).addClass(whichItem.class);
+                    if ((row == 0) && (col == 0)) {
+                        indexCell = $(nextRowObject).children()[totalSpanOffset];
+                        $(indexCell).attr("id", whichItem.name);
+                    } else {
+                        $(nextCell).attr("id", (nextRow) + "-" + (nextCol + col));
+                    }
+                }
             }
         }
-        // == set row/colspans on index cell to fill space
-        $(indexCell).attr("colSpan", whichItem.iW);
-        $(indexCell).attr("rowSpan", whichItem.iH);
-        $(indexCell).addClass(whichItem.class);
-        if (whichItem.type != "input") {
-            $(indexCell).attr("id", whichItem.name);
-        }
-    }
-
-    // ======= ======= ======= activateNextItem ======= ======= =======
-    Display.prototype.activateNextItem = function(whichItem) {
-        console.log("activateNextItem: " + whichItem.name);
+        display.toggleRowspans(whichItem, offsetR, offsetC, "on")
+        return indexCell;
     }
 
     // ======= ======= ======= unModifyGridRegion ======= ======= =======
-    Display.prototype.unModifyGridRegion = function(whichItem) {
+    Display.prototype.unModifyGridRegion = function(whichItem, offsetR, offsetC) {
         console.log("unModifyGridRegion: " + whichItem.name);
+
+        if (!offsetR) { offsetR = 0 };
+        if (!offsetC) { offsetC = 0 };
+        var tableRows = $("tr");
+        var regionType = whichItem.merge;
+
+        // == remove cells from merge area (check row/colspans in each row)
+        for (var row = 0; row < whichItem.iH; row++) {
+            nextRow = whichItem.iR + offsetR + row;
+            nextCol = whichItem.iC + offsetC;
+            nextRowObject = tableRows[nextRow];
+            colspans = this.checkColumnSpans(nextRowObject, nextRow, nextCol);
+            rowspans = this.checkRowSpans(nextRow, nextCol);
+            totalSpanOffset = nextCol - colspans - rowspans;
+            console.log("  totalSpanOffset: " + totalSpanOffset);
+
+            if (regionType == "merge") {
+                for (var col = 0; col < (whichItem.iW); col++) {
+                    if ((row == 0) && (col == 0)) {
+                        indexCell = $(nextRowObject).children()[totalSpanOffset];
+                        $(indexCell).remove();
+                        console.log("  $(indexCell).attr('id'): " + $(indexCell).attr('id'));
+                    }
+                    indexRowCell = $(nextRowObject).children()[totalSpanOffset - 1];
+                    for (var col = 0; col < whichItem.iW; col++) {
+                        var newCell = document.createElement("td");
+                        $(indexRowCell).after(newCell);
+                        $(newCell).addClass("cell");
+                        $(newCell).attr("id", (nextRow) + "-" + (nextCol + col));
+                    }
+                }
+            }
+        }
+        display.toggleRowspans(whichItem, offsetR, offsetC, "off")
+    }
+
+    // ======= ======= ======= deActivateNextItem ======= ======= =======
+    Display.prototype.deActivateNextItem = function(whichItem, indexCell) {
+        console.log("deActivateNextItem: " + whichItem.name);
+    }
+
+    // ======= ======= ======= activateNextItem ======= ======= =======
+    Display.prototype.activateNextItem = function(whichItem, indexCell) {
+        console.log("activateNextItem: " + whichItem.name);
+
+        var self = this;
+        var indexElement, tooltip;
+        var whichAction = whichItem.callback;
+
+        // ======= tooltips =======
+        $(indexCell).off("mouseenter").on("mouseenter", function(event){
+            // console.log("-- mouseenter");
+            indexElement = event.target;
+            tooltip = display.getTooltip(indexElement, "over");
+        });
+        $(indexCell).off("mouseout").on("mouseout", function(event){
+            // console.log("-- mouseout");
+            indexElement = event.target;
+            tooltip = display.getTooltip(indexElement, "out");
+        });
+
+        // ======= general =======
+        switch(whichAction) {
+            case "nextGameScreen":
+                $(indexCell).off("click").on("click", function(){
+                    console.log("");
+                    console.log("-- -- -- nextGameScreen -- -- -- ");
+                    game.nextGameScreen();
+                    $("#tooltips").text("enter player name and click ENTER");
+                });
+                break;
+            case "saveNewPlayer":
+                $(indexCell).off("click").on("click", function(){
+                    console.log("");
+                    console.log("-- -- -- saveNewPlayer -- -- -- ");
+                    game.saveNewPlayer();
+                    if (game.playerNamesArray.length < 3) {
+                        $("#tooltips").text("enter another player name or click START to begin");
+                    }
+                });
+                break;
+        }
+    }
+
+    // ======= ======= ======= getTooltip ======= ======= =======
+    Display.prototype.getTooltip = function(whichElement, overOut) {
+        console.log("getTooltip");
+
+        // $("#tooltips").text(nextTooltip);
     }
 
     // ======= ======= ======= checkRowSpans ======= ======= =======
     Display.prototype.checkRowSpans = function(whichRow, whichCol) {
         // console.log("checkRowSpans");
-        // console.log("  which_R/C: " + whichRow + "/" + whichCol);
 
         var rowspans = 0;
         var indexRow = 0;
         for (var col = 0; col < 18; col++) {
             if (col < whichCol) {
                 rowspanSpanObject = this.rowSpansArray[whichRow][col];
-                // console.log("  rowspan_R/C: " + rowspanSpanObject.R + "/" + rowspanSpanObject.C);
                 if (rowspanSpanObject.rspan == true) {
                     rowspans++;
                 }
             }
         }
-        // console.log("  rowspans: " + rowspans);
         return rowspans;
     }
 
@@ -368,14 +482,58 @@ function initGame() {
             nextColumnObject = $(whichRowObject).children()[col];
             nextColspan = $(nextColumnObject).attr('colSpan');
             nextColId = $(nextColumnObject).attr('id');
-            // console.log("  id/colspan " + nextColId + " /  " +  + nextColspan);
-            // if ((nextColspan > 1) && (col < (whichCol))) {
             if ((nextColspan > 1) && (col < (whichCol - colspans))) {
                 colspans += nextColspan - 1;
             }
         }
-        // console.log("  colspans: " + colspans);
         return colspans;
+    }
+
+    // ======= ======= ======= toggleRowspans ======= ======= =======
+    Display.prototype.toggleRowspans = function(whichItem, offsetR, offsetC, onOff) {
+        // console.log("toggleRowspans");
+
+        // == record rowspan elements
+        if (whichItem.iH > 1) {
+            for (var row = 0; row < whichItem.iH; row++) {
+                for (var col = 0; col < whichItem.iW; col++) {
+                    // rowspans not recorded for first row of multi-row area
+                    if (row != 0) {
+                        tableRow = whichItem.iR + offsetR + row;
+                        tableCol = whichItem.iC + offsetC + col;
+                        if (onOff == "on") {
+                            this.rowSpansArray[tableRow][tableCol].rspan = true;
+                        } else {
+                            this.rowSpansArray[tableRow][tableCol].rspan = false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // ======= ======= ======= reportRowspans ======= ======= =======
+    Display.prototype.reportRowspans = function(whichRow) {
+        console.log("reportRowspans ------- ------- row: " + whichRow);
+
+        // == display rowspan status for targeted row
+        targetRow = 7;
+        if (whichRow == targetRow) {
+            for (var row = 0; row < this.tableRowspansArray.length; row++) {
+                nextRow = this.tableRowspansArray[row];
+                if (row == whichRow) {
+                    console.log("   nextRow.length: " + nextRow.length);
+                    for (var col = 0; col < nextRow.length; col++) {
+                        nextCol = nextRow[col];
+                        if (nextCol.rspan == true) {
+                            console.log("  rowspan TRUE: " + row + "/" +  col);
+                        } else {
+                            console.log("  rowspan FALSE: " + row + "/" +  col);
+                        }
+                    }
+                }
+            }
+        }
     }
 
     // ======= ======= ======= initRowSpans ======= ======= =======
@@ -415,18 +573,104 @@ function initGame() {
         this.deckPointsArray = [];
         this.currentPlayer = 0;
         this.currentScreen = null;
-        this.currentSubScreen = null;
-        this.playerNames = [];
-        this.allPlayers = [player1, player2, player3, dealer];
-        this.allScreens = [splash, nameEnter, enterPlay];
-        // this.allSubScreens = [player1_scr, player2_scr, player3_scr, dealer_scr, scoreboard_scr];
+        this.currentSubscreen = null;
+        this.playerNamesArray = [];
+        this.playerObjectsArray = [player1, player2, player3, dealer];
+        this.screenObjectsArray = [splash, nameEnter, enterPlay];
+        this.subcreenObjectsArray = [player1_scr, player2_scr, player3_scr, dealer_scr, scoreboard_scr];
         this.playerStates = ["inactive", "active", "placeBets", "hitMeHoldMe", "turnOver", "handOver"]
-        this.allSubScreens = [player1_scr];
         this.onesBet = 0;
         this.fivesBet = 0;
         this.tensBet = 0;
     }
 
+
+    // ======= ======= ======= nextGameScreen ======= ======= =======
+    Game.prototype.nextGameScreen = function() {
+        console.log("nextGameScreen");
+        display.getNextScreen();
+    }
+
+    // ======= ======= ======= saveNewPlayer ======= ======= =======
+    Game.prototype.saveNewPlayer = function() {
+        console.log("saveNewPlayer");
+
+        playerName = $('#playerNameInput').val();
+        if (playerName) {
+            var playerCount = this.playerNamesArray.length;
+            if (playerCount < 3) {
+                newPlayer = this.playerObjectsArray[playerCount];
+                this.playerNamesArray.push(newPlayer.name);
+                var playerCount = this.playerNamesArray.length;
+                display.makeSubscreen(playerCount);
+                // this.updatePlayerNames(newPlayer, playerCount);
+                $("#tooltips").text("Enter another name or press 'start'");
+            }
+            if (playerCount == 1) {
+                this.nextGameScreen();
+            }
+            if (playerCount == 3) {
+                // $("#tooltips").text("Max of 3 players.  Start game!");
+                display.makeSubscreen(4);
+                this.currentPlayer = game.playerObjectsArray[0];
+                // this.updatePlayerNames(this.dealer, 4);
+                this.nextGameScreen();
+                $("#tooltips").text("click DEAL to deal cards");
+            }
+            $("#playerNameInput").val("");
+        } else {
+            $("#tooltips").text("Enter name or press 'start'");
+        }
+    }
+
+    // ======= ======= ======= makeSubscreen ======= ======= =======
+    Display.prototype.makeSubscreen = function(playerIndex) {
+        console.log("makeSubscreen");
+
+        // identify where we are in the game (prev screen/next screen/selected screen)
+        // use current screen name to identify current screen index
+        // increment screen index to get next screen object
+
+        // create array of screen items
+        var nextItemType, items, nextItem, nextItemName;
+        var tempItemsNext = [];
+        var tempNamesNext = [];
+        nextScreen = game.subcreenObjectsArray[playerIndex - 1];
+        itemTypesArray = ["bg", "btn", "slider", "text", "input", "image"];
+        for (var i = 0; i < itemTypesArray.length; i++) {
+            nextItemType = itemTypesArray[i];
+            if (nextScreen[nextItemType]) {
+                items = Array.isArray(nextScreen[nextItemType]);
+                if (items) {
+                    for (var j = 0; j < nextScreen[nextItemType].length; j++) {
+                        nextItem = nextScreen[nextItemType][j];
+                        nextItemName = nextItem.name;
+                        tempItemsNext.push(nextItem);
+                        tempNamesNext.push(nextItemName);
+                    }
+                } else {
+                    nextItem = nextScreen[nextItemType];
+                    nextItemName = nextItem.name;
+                    tempItemsNext.push(nextItem);
+                    tempNamesNext.push(nextItemName);
+                }
+            }
+        }
+
+        for (var j = 0; j < tempItemsNext.length; j++) {
+            nextItem = tempItemsNext[j];
+            nextType = nextItem.type;
+            indexCell = display.modifyGridRegion(nextItem);
+            if (nextType == "input") {
+                newTextInput = "<input id='" + nextItem.name + "Input' class='" + nextItem.class + "' type='text' value='Tom'>"
+                $(indexCell).append(newTextInput);
+                $(newTextInput).attr("id", nextItem.name);
+            }
+            if ((nextType == "btn") || (nextType == "slider")) {
+                display.activateNextItem(nextItem, indexCell);
+            }
+        }
+    }
 
     // ======= ======= ======= ======= ======= initialize ======= ======= ======= ======= =======
     // ======= ======= ======= ======= ======= initialize ======= ======= ======= ======= =======
@@ -443,7 +687,7 @@ function initGame() {
 
     display.initRowSpans();
     display.getNextScreen();
-    display.getNextSubScreen();
+    // display.getNextsubscreen();
 
 }
 
